a2ce2d932f082e53d38f390bede1f40fbe70b2eb
YoonJunHyeon <qmdldos@gmail.com>
2023-11-08 10:42:45 +0900

    object.key와 forEach에 대한 소개

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-
+/**
+ * 
+ * @param {*} tagName 
+ * @param {*} styleObject 
+ * @returns DOM element
+ * 
+ * ? Object.key()는
+ * ? 객체의 key 값들을 배열로 반환한다.
+ * 
+ * ? forEach()는
+ * ? 배열의 각 요소에 대해 제공된 함수를 차례대로 한번씩 실행한다.
+ * ? for()문을 '배열에서' 사용하게끔 추상화된 js문법이다.
+ */
 
 export default function(tagName, styleObject) {
   const element = document.createElement(tagName);
