b3c84476889afa5ee22444f504342ae60813466f
YoonJunHyeon <qmdldos@gmail.com>
2023-11-08 10:39:48 +0900

    object를 받아오면 자동으로 component의 style이 지정된다.

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+
+
 export default function(tagName, styleObject) {
   const element = document.createElement(tagName);
   let styleObjectKeyArray = Object .keys(styleObject);
+  styleObjectKeyArray.forEach((key)=>{
+    element.style[key] = styleObject[key];
+  });
+  return element;
 }
